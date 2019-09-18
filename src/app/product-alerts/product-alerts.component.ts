56d547fdf33194a7b953559ffddff89739566b5c
TAKETODAY <2228592280@qq.com>
2019-09-18 12:53:17 +0800

    2 files modified, 1 added

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,4 +1,6 @@
+
 import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core';
 
 @Component({
   selector: 'app-product-alerts',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductAlertsComponent implements OnInit {
 
+  @Input() product;
+
+
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
